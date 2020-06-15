35ffca82a166083949ccd483b784dd57800c9796
Kay Khahima <42249295+kaykhahima@users.noreply.github.com>
2020-06-15 18:11:06 +0300

    changed smtp username

--- contact/index.php
+++ contact/index.php
@@ -2174,8 +2174,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kaykhahima@gmail.com';                 // SMTP username
-$mail->Password = 'KayCortez!!3!';                           // SMTP password
+$mail->Username = 'ikaykhahima@gmail.com';                 // SMTP username
+$mail->Password = 'MpA0o3i4fvD2iipo';                           // SMTP password
 $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                   // TCP port to connect to
 
